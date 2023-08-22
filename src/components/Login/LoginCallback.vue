a94c7e4e6474186fa6fe9f4ca3618de2693c26bd
xongjaemin <woals99@gmail.com>
2023-08-23 00:20:53 +0900

    etc: 로그인 테스트..

--- src/components/Login/LoginCallback.vue
+++ src/components/Login/LoginCallback.vue
@@ -5,11 +5,16 @@ import { useRoute } from "vue-router";
 import { verifySocialId } from "@/api/api";
 const route = useRoute();
 
+const verify = async (authCode: string) => {
+  const response = await verifySocialId(authCode);
+  console.log(response.data);
+};
+
 onMounted(() => {
   const authorizationCode = route.query.code;
   if (authorizationCode !== null) {
     // console.log(authorizationCode.toString());
-    verifySocialId(authorizationCode.toString());
+    verify(authorizationCode.toString());
   }
 });
 </script>
